ece64cc43d4bd1ae1cc4b9b58dae02abbf62da2f
Antti Helin <antti.helin@tuni.fi>
2023-02-24 17:02:35 +0200

    Harjoitus 6 t1 ja t2

--- Assets/Scenes/InputReader.cs
+++ Assets/Scenes/InputReader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using static Harjoitukset.H6T1;
 
 namespace Harjoitukset
 {
@@ -12,12 +13,47 @@ namespace Harjoitukset
         private bool jumping = false;
 
 
+        public enum ControlMethod
+        {
+            VirtualJoystick,
+            Tap
+        }
+
+        private PlayerInput playerInput;
+
+        private ControlMethod currentControlMethod = ControlMethod.Tap;
+        /*
         public void OnMove(InputAction.CallbackContext context)
         {
             // Luetaan käyttäjän hahmoa liikuttava syöte muuttujaan.
             moveInput = context.ReadValue<Vector2>();
             //Debug.Log($"Syöte: {moveInput}");
         }
+        */
+        private void Awake()
+        {
+            playerInput = GetComponent<PlayerInput>();
+        }
+
+        private void Start()
+        {
+            // HACK: Estä PlayerInputtia vaihtamasta kontrolliskeemaa 
+            if (playerInput.currentActionMap.name == "Game")
+            {
+                // Jos virtualjoystick käytössä estetään unityä vaihtamasta kontrolliskeemaa
+                playerInput.neverAutoSwitchControlSchemes = true;
+                playerInput.SwitchCurrentControlScheme(Gamepad.current);
+            }
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            currentControlMethod = ControlMethod.VirtualJoystick;
+            //Luetaan käyttäjän hahmoa liikuttava syöte muuttujaan
+            moveInput = context.ReadValue<Vector2>();
+            Debug.Log($"Syöte: {moveInput}");
+        }
+
 
         public void OnJump(InputAction.CallbackContext context)
         {
@@ -33,10 +69,26 @@ namespace Harjoitukset
         }
 
 
+
+        public Vector2 GetMoveInput()
+        {
+            if (currentControlMethod == ControlMethod.VirtualJoystick)
+            {
+                return this.moveInput;
+            }
+
+            
+            return this.moveInput.normalized;
+
+        }
+
+
+        /*
         public Vector2 GetMoveInput()
         {
             return moveInput;
         }
+        */
 
         public bool GetJump()
         {
